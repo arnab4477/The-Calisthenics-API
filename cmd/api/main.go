180099650c22878b4461f1a40e4119f602bf77dc
arnab4477 <arnabsahareal@gmail.com>
2022-11-07 20:24:13 +0530

    Set up Postgres database and created a connection pool for the movement informations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -15,6 +19,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env string
+	db struct{
+		dsn string
+	}
 }
 
 
@@ -32,10 +39,21 @@ func main() {
 	// Set flags and their default values
 	flag.IntVar(&cfg.port, "port", 7000, "The API port")
 	flag.StringVar(&cfg.env, "env", "development", "Enviroment (development | staging | production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("PARKOUR_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
 
 	// Logger function for customized logging
 	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+
+	// Create a database connection pool
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Make sure the connection to the pool closes before the main()
+	defer db.Close()
+	logger.Printf("Database connecton establisted")
 
 	// An instance of the application struct
 	app := &application {
@@ -55,6 +73,29 @@ func main() {
 	// Start the server
 	logger.Printf("Starting %s server on port %s", cfg.env, srv.Addr)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+// This function returns a SQLdb connection pool
+func openDB(cfg config) (*sql.DB, error) {
+	// Create an empty connection pool with the config DSN
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a context with 5 second time out
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	// reate a new connection with the context reatd above
+	// If the connecto is not established within 5 seconds, this will return an error
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the connection pool
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/arnab4477/Parkour_API
 
 go 1.19
 
-require github.com/julienschmidt/httprouter v1.3.0 // direct
+require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/lib/pq v1.10.2
